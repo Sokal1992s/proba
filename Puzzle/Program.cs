b51aad4f1541622248bdbf732fab03759c6f0e39
radosinska <radosinska@wp.pl>
2015-03-25 15:13:47 +0100

    new combination

--- Puzzle/Program.cs
+++ Puzzle/Program.cs
@@ -15,29 +15,39 @@ namespace Puzzle
 
             //for (int i = 0; i < 9; i++)
             //    startState.PuzzleState[i] = i;
+            //to sie liczy długo..
+            //startState.PuzzleState[0] = 4;
+            //startState.PuzzleState[1] = 1;
+            //startState.PuzzleState[2] = 0;
+            //startState.PuzzleState[3] = 7;
+            //startState.PuzzleState[4] = 6;
+            //startState.PuzzleState[5] = 5;
+            //startState.PuzzleState[6] = 2;
+            //startState.PuzzleState[7] = 3;
+            //startState.PuzzleState[8] = 8;
 
-            startState.PuzzleState[0] = 4;
-            startState.PuzzleState[1] = 1;
+            startState.PuzzleState[0] = 1;
+            startState.PuzzleState[1] = 6;
             startState.PuzzleState[2] = 0;
-            startState.PuzzleState[3] = 7;
-            startState.PuzzleState[4] = 6;
-            startState.PuzzleState[5] = 5;
-            startState.PuzzleState[6] = 2;
-            startState.PuzzleState[7] = 3;
-            startState.PuzzleState[8] = 8;
+            startState.PuzzleState[3] = 5;
+            startState.PuzzleState[4] = 7;
+            startState.PuzzleState[5] = 2;
+            startState.PuzzleState[6] = 4;
+            startState.PuzzleState[7] = 8;
+            startState.PuzzleState[8] = 3;
 
             State endState = new State();
             //for (int i = 0; i < 9; i++)
             //    endState.PuzzleState[i] = i;
-            startState.PuzzleState[0] = 1;
-            startState.PuzzleState[1] = 2;
-            startState.PuzzleState[2] = 3;
-            startState.PuzzleState[3] = 4;
-            startState.PuzzleState[4] = 5;
-            startState.PuzzleState[5] = 6;
-            startState.PuzzleState[6] = 7;
-            startState.PuzzleState[7] = 8;
-            startState.PuzzleState[8] = 0;
+            endState.PuzzleState[0] = 1;
+            endState.PuzzleState[1] = 2;
+            endState.PuzzleState[2] = 3;
+            endState.PuzzleState[3] = 4;
+            endState.PuzzleState[4] = 5;
+            endState.PuzzleState[5] = 6;
+            endState.PuzzleState[6] = 7;
+            endState.PuzzleState[7] = 8;
+            endState.PuzzleState[8] = 0;
 
             Problem P = new Problem(startState, endState);
             FringeFifo F = new FringeFifo();
